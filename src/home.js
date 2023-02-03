e4ec1addc9584962eb19b8460ea37fb4a5689348
yasmeinelkenany <yasmeinelkenany@gmail.com>
2023-02-03 21:15:58 +0200

    Edit styles.css index.html

---
+++ src/home.js
@@ -0,0 +1,9 @@
+
+export default function home() {
+    console.log("hello hey")
+}
+
+export function content() {
+    let content = document.getElementById("content");
+    content.style.backgroundColor = "#000"
+}
